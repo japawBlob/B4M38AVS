d54e864aaeb9d06095f0a45ae8d90889b4ebe90c
JakubJíra <jirajak2@fel.cvut.cz>
2023-05-29 09:04:27 +0200

    cleaning repository, renaming projects, added DDS

--- 01_AVS-SC-KIT-Led_Blick/SC-IDE/build/SC-KIT/CodeSourceryARM-Windows/_ext/513150005/iprintf.o.d
+++
@@ -1,2 +0,0 @@
-build/SC-KIT/CodeSourceryARM-Windows/_ext/513150005/iprintf.o: \
- ../../../../Utilities/Third_Party/System/iprintf.c
--- 03_AVS-SC-KIT-ENCODER/SC-IDE/build/SC-KIT/CodeSourceryARM-Linux-x86/_ext/513150005/iprintf.o.d
+++
@@ -1,2 +0,0 @@
-build/SC-KIT/CodeSourceryARM-Linux-x86/_ext/513150005/iprintf.o: \
- ../../../../Utilities/Third_Party/System/iprintf.c
